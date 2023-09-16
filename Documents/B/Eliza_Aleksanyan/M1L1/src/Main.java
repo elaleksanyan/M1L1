2716b93e50716f1c2f7ef04bd72aff141a1d1a21
EmmaManukyan <emmamanukyan2006@gmail.com>
2023-09-16 14:34:14 +0400

    first commit

---
+++ Documents/B/Eliza_Aleksanyan/M1L1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for(int i = 0; i < 5; i++){
+            int x = i * 2;
+            System.out.println("x = " + x);
+            System.out.println();
+        }
+    }
+}
